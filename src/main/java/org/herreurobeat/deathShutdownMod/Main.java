787ca27ea5accca7335915029558d35d2f4b3533
3urobeat <35304405+HerrEurobeat@users.noreply.github.com>
2022-03-22 12:09:07 +0100

    Added basic mod structure

---
+++ src/main/java/org/herreurobeat/deathShutdownMod/Main.java
@@ -0,0 +1,10 @@
+package org.herreurobeat.deathShutdownMod;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Main implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+    }
+}
